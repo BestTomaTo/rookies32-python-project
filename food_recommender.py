1c616ad30b8fc5ad2f031463b6d64bebfb3e626e
hs-kim00 <hsluigi0614@naver.com>
2026-05-06 20:39:31 +0900

    알레르기 설정 기능과 음식 추천 기능을 만듦. 피드백 요청 바람

--- food_recommender.py
+++ food_recommender.py
@@ -9,8 +9,7 @@ load_dotenv()
 SPOON_API_KEY = os.getenv("SPOON_API_KEY")
 spoon_url = "https://api.spoonacular.com/recipes/complexSearch"
 spoon_headers = {
-    'Content-Type': 'application/json',
-    'apiKey': SPOON_API_KEY
+    'x-api-key': SPOON_API_KEY
 }
 
 # 알레르기 관련 설정
@@ -67,17 +66,40 @@ def intolerances_option():
         print(e)
 
 def recommend_food():
-    # ingredients_list = read_food() # 재료를 불러옴 출력 예시? ([egg, sugar])
+    ingredients_list = "egg,sugar,carrot,apple,jicama,lime"# read_food() # 재료를 불러옴 출력 예시? ([egg, sugar])
     # ingredients = ",".join(ingredients_list)
     intolerances_list = intolerances_instance.return_intolerances_list() # 알레르기 정보 조회?
     intolerances = ",".join(intolerances_list)
 
+    print(ingredients_list, intolerances) # 디버깅용 평소에는 주석 처리
+
     params = {
         "intolerances": intolerances,
-        # "includeIngredients": ingredients_list
+        "includeIngredients": ingredients_list,
+        "number": 5,
+        "fillIngredients": True
     }
 
-    # response = requests.get(spoon_url, headers=spoon_headers, params=params)
+    response = requests.get(spoon_url, headers=spoon_headers, params=params)
+    # 확인용(json형태로 response 확인)
+    with open("result.json", "w", encoding="utf-8") as f:
+        f.write(response.text)
+    recommend_food_data = response.json().get("results", [])
+    print(recommend_food_data)
+
+    print("=" * 60)
+    print("현재 재료로 만들 수 있는 요리는 다음과 같습니다.")
+    for i in range(len(recommend_food_data)):
+        used_ingredients = recommend_food_data[i].get("usedIngredients", [])
+        unused_ingredients = recommend_food_data[i].get("missedIngredients", [])
+        print(f"{i+1}. {recommend_food_data[i].get("title", None)}")
+        print("냉장고에서 사용되는 재료: ")
+        for i in range(len(used_ingredients)):
+            print(f"{i+1}. {used_ingredients[i].get("name", None)}")
+        print("냉장고에 없는 재료: ")
+        for i in range(len(unused_ingredients)):
+            print(f"{i+1}. {unused_ingredients[i].get("name", None)}")
+        print("-" * 60)
 
 while True:
     # 단순 기능 출력
